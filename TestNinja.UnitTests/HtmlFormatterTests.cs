365187cfd2ad8fb677a05eee1875081ee76d3e89
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-04-29 22:20:57 +0530

    Testing String with Does

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific assertions
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            // More general in case we are testing exception message or string that can change over time
+
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
