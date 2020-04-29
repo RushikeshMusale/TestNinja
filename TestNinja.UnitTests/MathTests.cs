b0bd7fc68738634370cae251999b00dd7ac447f1
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-04-29 19:13:19 +0530

    Add project files.

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfTwoNumbers()
+        {
+            Math math = new Math();
+
+            var result = math.Add(1, 3);
+
+            Assert.That(result, Is.EqualTo(4));
+        }   
+        
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsFirstArgument()
+        {
+            Math math = new Math();
+            var result = math.Max(3, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsSecondArgument()
+        {
+            Math math = new Math();
+            var result = math.Max(2, 3);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsSameArgument()
+        {
+            Math math = new Math();
+            var result = math.Max(3, 3);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+   // [TestClass] MS Test
+    public class ReservationTests
+    {
+        [Test]
+       // [TestMethod] MS Test
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //arrange
+            var reservation = new Reservation();
+
+            //act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //assert
+            //Assert.IsTrue(result); // MS Test default
+            Assert.That(result, Is.True);
+
+        }
+
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingReservation_ReturnsTrue()
+        {
+            //arrange
+            var reservation = new Reservation();
+
+            var user = new User();
+            reservation.MadeBy = user;            
+
+            //act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnsTrue()
+        {
+            //arrange
+            var reservation = new Reservation() { MadeBy = new User() };
+                     
+
+            //act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+}
