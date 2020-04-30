68eeac9dad031127d40b23c3183512dd4c86762e
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-04-30 11:03:48 +0530

    Testing Method that throws exception

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -21,5 +21,22 @@ namespace TestNinja.UnitTests
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
 
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Log_InvalidInput_ThrowsArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            // if we directly call method here then it will throw exception & test case will fail
+            // So we use lamda expression
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+            // Another way is use generic excpetion for custom exception
+
+            Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());           
+        }
+
     }
 }
