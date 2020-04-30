4488feea65ea74c4bfb48fa97afe99a8ffd0639e
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-04-30 10:28:48 +0530

    Testing return type by TypeOf & InstanceOf

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnsNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            //two ways to check types
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // InstanceOf returns true if the result is class or subclass of the type
+            Assert.That(result, Is.InstanceOf<NotFound>());
+
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnsOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
