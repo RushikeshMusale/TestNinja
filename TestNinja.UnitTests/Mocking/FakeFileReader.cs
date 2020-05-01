4fcaaf8d8b5b597d4eedb09ec46403a3b329454e
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-05-01 13:21:28 +0530

    Testing Class with External Dependency - Paremeter DI Create FakeObject Manually

---
+++ TestNinja.UnitTests/Mocking/FakeFileReader.cs
@@ -0,0 +1,17 @@
+﻿using TestNinja.Mocking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
