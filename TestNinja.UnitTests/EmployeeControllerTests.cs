a7051f11d1ef0a14c7948d4d912b42b86cdb179e
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-05-02 18:00:03 +0530

    Testing External Dependency - Employee Controller - Part 2

---
+++ TestNinja.UnitTests/EmployeeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class EmployeeControllerTests
+    {
+        private Mock<IEmployeeStorage> _storage;
+        private EmployeeController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _storage = new Mock<IEmployeeStorage>();
+            _controller = new EmployeeController(_storage.Object);
+        }
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnsRedirectResult()
+        {
+            
+            var result = _controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeletesEmployee()
+        {
+            var result = _controller.DeleteEmployee(1);
+
+            _storage.Verify(s => s.Remove(1));
+        }
+    }
+}
