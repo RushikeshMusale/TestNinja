a1388a6a5d2f89c7b9b774faa898fff1f794c607
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-05-02 12:38:47 +0530

    MockAbuse - Part1 - Best solution for Product class

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomers_Apply30PercentDicsount()
+        {
+            var product = new Product() { ListPrice = 100 };
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
