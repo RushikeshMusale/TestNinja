416a56fb9c5e208a9cb4671e33a5f8d95b4b8c13
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-05-02 12:21:54 +0530

    Mocking abstract classs & normal class

---
+++ TestNinja.UnitTests/Mocking/TestForMoqLib/MoqTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Mocking.TestForMoqLib
+{
+    [TestFixture]
+    class MoqTests
+    {
+        [Test]
+        public void Moq_AbstractClass()
+        {
+            var abstractClass = new Mock<AbstractClass>();
+            abstractClass.Setup(ac => ac.ReturnHello()).Returns("hello");
+            var res = abstractClass.Object.ReturnHello();
+            Assert.That(res, Does.Contain("hello").IgnoreCase);
+        }
+
+        [Test]
+        public void Moq_SimpleClass()
+        {
+            var simpleClass = new Mock<SimpleClass>();
+            simpleClass.Setup(sc => sc.ReturnsHello()).Returns("hello1");
+
+            var res = simpleClass.Object.ReturnsHello();
+            Assert.That(res, Does.Contain("hello1"));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Mocking/TestForMoqLib/SimpleClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Mocking.TestForMoqLib
+{
+   public  class SimpleClass
+    {
+        public SimpleClass(string parameter)
+        {
+
+        }
+
+        // it does need default constructor to create a mock object
+        public SimpleClass()
+        {
+
+        }
+
+        // Only methods abstract or virtual can be mocked
+        public virtual string  ReturnsHello()
+        {
+            return "hello";
+        }
+    }
+}
