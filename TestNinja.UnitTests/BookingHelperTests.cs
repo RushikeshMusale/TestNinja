5bc361f62a246d5c2283d685370b6ac113f620f9
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-05-03 18:01:27 +0530

    Testing BookingHelper - Part3  - BookingStartsAndEndsBeforeExistingBooking_ReturnEmptyString  - Refactoring

--- TestNinja.UnitTests/BookingHelperTests.cs
+++ TestNinja.UnitTests/BookingHelperTests.cs
@@ -13,37 +13,59 @@ namespace TestNinja.UnitTests
     class BookingHelper_OverlappingBookingExistsTests
     {
         private Mock<IBookingRepository> _bookingRepository;
+        private Booking _existingBooking;
 
         [SetUp]
         public void Setup()
         {
             _bookingRepository = new Mock<IBookingRepository>();
-            
+
+            _existingBooking = new Booking
+            {
+                Id = 2,
+                ArrivalDate = ArriveOn(20, 1, 14),
+                DepartureDate = DepartOn(20, 1, 20),
+                Reference = "a"
+            };
+
+            _bookingRepository.Setup(br => br.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+               _existingBooking
+            }.AsQueryable<Booking>);
         }
 
         [Test]
         public void BookingStartsAndEndsBeforeExistingBooking_ReturnEmptyString()
-        {
-            _bookingRepository.Setup(br => br.GetActiveBookings(1)).Returns(new List<Booking>
-            {
-                new Booking
-                {
-                    Id =2,
-                    ArrivalDate = new DateTime(20,1,14,14,0,0),
-                    DepartureDate = new DateTime(20, 1, 20, 10,0,0),
-                    Reference = "a"                    
-                }
-            }.AsQueryable<Booking>);
-
+        {          
             string result = BookingHelper.OverlappingBookingsExist(
                 new Booking
                 {
                     Id = 1,
-                    ArrivalDate = new DateTime(20,1, 10, 14,0,0),
-                    DepartureDate = new DateTime(20,1,13, 10,0,0 )
+                    ArrivalDate = Before(_existingBooking.ArrivalDate, days: 5),
+                    DepartureDate = Before(_existingBooking.ArrivalDate)
                 }, _bookingRepository.Object);
 
             Assert.That(result, Is.Empty);
+        }
+
+        private DateTime Before(DateTime dateTime, int days = 1)
+        {
+            return dateTime.AddDays(-days);
+        }
+
+        private DateTime After(DateTime dateTime)
+        {
+            return dateTime.AddDays(1);
+        }
+
+        private DateTime ArriveOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 14, 0, 0);
+        }
+
+        private DateTime DepartOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 10, 0, 0);
         }
     }
 }
